81c60269779e3eddaf1530a934b6ed58a55ed7b6
IndraHfid <indrahafidsaputra@gmail.com>
2026-01-17 16:39:16 +0700

    Membuat konfigurasi route navigasi aplikasi

---
+++ app/src/main/java/com/example/projectahirpam/uicontroller/navigation/Routes.kt
@@ -0,0 +1,13 @@
+package com.example.projectahirpam.uicontroller.navigation
+
+object Routes {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val DASHBOARD = "dashboard"
+    const val BARANG = "barang"
+    const val KATEGORI = "kategori"
+    const val STOK = "stok"
+    const val STOK_MASUK = "stok_masuk"
+    const val STOK_KELUAR = "stok_keluar"
+    const val LAPORAN = "laporan"
+}
